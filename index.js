341a6ac6e716b0310d0915bb701b5de0c4c2759f
unknown <abhiippili1234@gmail.com>
2024-02-22 13:58:40 +0530

    socket.io server added

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const { createServer } = require("node:http");
 const path = require("path");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
+});
+
+io.on("connection", (socket) => {
+  console.log("someone has been connected");
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+  socket.on("disconnect", () => {
+    console.log("disconnected");
+  });
 });
 
 server.listen(3000, () => {
